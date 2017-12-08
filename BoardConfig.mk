d7d81d6c5dd4bcc6bf21e5bc8d17b57416734f08
Umang Raghuvanshi <u@umangis.me>
2017-12-08 07:43:21 +0000

    [sepolicy]: Add file_contexts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,6 +198,8 @@ USE_SENSOR_MULTI_HAL := true
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
